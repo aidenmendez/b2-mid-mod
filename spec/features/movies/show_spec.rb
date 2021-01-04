b98e4a701c6b5fae04656d4b51edccf4e1f61f13
Aiden Mendez <aidenmendez@gmail.com>
2021-01-04 10:41:11 -0700

    Create movies controller and show view, update routes

---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "As a user" do
+  describe "When I visit a movie's show page" do
+    before(:each) do
+      @studio1 = Studio.create(name:"Universal Studios", location:"Orlando, FL")
+      @shrek = Movie.create(title: "Shrek", creation_year: 2001, genre: "Comedy", studio_id: @studio1.id)
+      @hathaway = Actor.create(name: "Anne Hathaway", age: 38)
+      @pit = Actor.create(name: "Brad Pit", age: 57)
+      @andrews = Actor.create(name: "Julie Andrews", age: 85)
+      MovieActor.create(actor: @hathaway, movie: @shrek)
+      MovieActor.create(actor: @pit, movie: @shrek)
+      MovieActor.create(actor: @andrews, movie: @shrek)
+    end
+    it "I see the movie's title, creation year, and genre," do
+      visit "/movies/#{@shrek.id}"
+      expect(page).to have_content(@shrek.title)
+      expect(page).to have_content(@shrek.creation_year)
+      expect(page).to have_content(@shrek.genre)
+    end
+    it "and a list of all its actors from youngest to oldest" do
+      
+    end
+    it "And I see the average age of all of the movie's actors" do
+
+    end
+  end
+end
