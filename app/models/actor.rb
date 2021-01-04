6f567ef28cc295ef3fa04df89af183796538b70e
Aiden Mendez <aidenmendez@gmail.com>
2021-01-04 11:59:06 -0700

    Create actor search functionality

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,4 +9,8 @@ class Actor < ApplicationRecord
   def self.average_age(actors)
     actors.average(:age)
   end
+
+  def self.name_search(search_term)
+    Actor.where(name: search_term).first
+  end
 end
